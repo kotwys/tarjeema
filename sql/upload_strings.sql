5a511f3fe930887b99d5975c36a72d35380ea154
Kočyš Mikajlo <52920928+kotwys@users.noreply.github.com>
2025-05-17 22:36:57 +0400

    Fix uploading strings for different projects

--- sql/upload_strings.sql
+++ sql/upload_strings.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE upload_strings ( project_id  integer
+CREATE OR REPLACE PROCEDURE upload_strings ( pid         integer
                                            , string_data json )
 LANGUAGE SQL
 AS $$
@@ -6,10 +6,10 @@ AS $$
   USING ( SELECT *
             FROM json_each_text ( string_data )
         ) AS j
-     ON ( s.project_id , s.string_name ) = ( project_id , j.key )
+     ON ( s.project_id , s.string_name ) = ( pid , j.key )
    WHEN MATCHED THEN UPDATE SET string_text = j.value
-   WHEN NOT MATCHED BY SOURCE AND s.project_id = project_id THEN DELETE
+   WHEN NOT MATCHED BY SOURCE AND s.project_id = pid THEN DELETE
    WHEN NOT MATCHED BY TARGET THEN
         INSERT ( project_id , string_name , string_text )
-        VALUES ( project_id , j.key , j.value );
+        VALUES ( pid , j.key , j.value );
 $$;
